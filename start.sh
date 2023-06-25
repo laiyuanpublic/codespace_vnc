66bf1159b3847df124120dd682ce8a459faa7a26
Kai Shu Lin <118428741+laiyuanpublic@users.noreply.github.com>
2023-06-25 08:36:30 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-mkdir /home/headless/.vnc && echo 123456 | vncpasswd -f > /home/headless/.vnc/passwd
+mkdir /home/headless/.vnc && echo 123456 | vncpasswd -f > /home/headless/.vnc/passwd && chmod 0600 /home/headless/.vnc/passwd
 vncserver :0 &
 /usr/libexec/noVNCdim/utils/novnc_proxy &
